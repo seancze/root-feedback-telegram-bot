8488260ed8f998878fdb650d5ad13fad43eb05e8
Sean <seancze@gmail.com>
2022-06-30 01:11:21 +0800

    refactor: remove heroku config settings

--- bot.py
+++ bot.py
@@ -370,11 +370,11 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    # updater.start_polling()
-    updater.start_webhook(listen="0.0.0.0",
-                            port=int(PORT),
-                            url_path=TOKEN)
-    updater.bot.setWebhook('https://sutd-root-bot.herokuapp.com/' + TOKEN)
+    updater.start_polling()
+    # updater.start_webhook(listen="0.0.0.0",
+    #                         port=int(PORT),
+    #                         url_path=TOKEN)
+    # updater.bot.setWebhook('https://sutd-root-bot.herokuapp.com/' + TOKEN)
 
     # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
